e58edbe6d44eed52798310060830f873d8bd3730
Florian Salihovic <florian.salihovic@me.com>
2014-05-12 17:59:47 +0200

    #8 Adding components for adding/editing visit information.

--- pet-clinic-ui/src/main/resources/SLING-INF/apps/petclinic/components/pages/visits/add.groovy
+++ pet-clinic-ui/src/main/resources/SLING-INF/apps/petclinic/components/pages/visits/add.groovy
@@ -1,13 +1,3 @@
-import org.apache.sling.api.resource.ValueMap
-
-def suffix = request.getRequestPathInfo().getSuffix()
-def resourceResolver = resource.getResourceResolver()
-
-def petResource = resourceResolver.getResource(suffix)
-def petProps = petResource.adaptTo(ValueMap.class)
-def ownerResource = petResource.getParent().getParent()
-def ownerProps = ownerResource.adaptTo(ValueMap.class)
-
 sling.include(resource, 'petclinic/components/header')
 sling.include(resource, 'petclinic/components/navigation')
 
@@ -19,37 +9,7 @@ markupBuilder.html {
           h1(class: 'ui header', 'Add Pet')
         }
       }
-      form(class: 'ui form', role: 'form', action: "${suffix}/visits/*", method: 'POST') {
-        div(class: 'field') {
-          label(for: 'firstName', 'First Name:')
-          input(id: 'firstName', name: 'firstName', type: 'text', readonly: 'true',
-              value: "${ownerProps.get('firstName')} ${ownerProps.get('lastName')}")
-        }
-        div(class: 'field') {
-          label(for: 'name', 'Name:')
-          input(id: 'petName', name: 'petName', type: 'text', placeholder: 'Name', readonly: 'true',
-              value: "${petProps.get('name')}")
-        }
-        div(class: 'field') {
-          label(for: 'birthDate', 'Birth Date:')
-          input(id: 'birthDate', name: 'birthDate', type: 'text', placeholder: 'dd/MM/yy', readonly: 'true',
-              value: "${petProps.get('birthDate')}")
-        }
-        div(class: 'field') {
-          label(for: 'date', 'Date:')
-          input(id: 'date', name: 'date', type: 'text', placeholder: 'dd/MM/yy')
-        }
-        div(class: 'field') {
-          label(for: 'description', 'Description:')
-          input(id: 'description', name: 'description', type: 'text', placeholder: 'Description')
-        }
-        input(type: 'hidden', name: 'sling:resourceType', value: 'petclinic/components/pages/owners/pets/visits/visit')
-        input(type: 'hidden', name: ':nameHint', value: 'visit')
-        input(type: 'hidden', name: ':redirect',
-            value: "/content/petclinic/en/owners.detail.html${ownerResource.getPath()}")
-        input(type: 'hidden', name: '_charset_', value: 'UTF-8')
-        button(type: 'submit', class: 'ui blue submit button', 'Save')
-      }
+      sling.include(resource, 'petclinic/components/visits')
     }
   }
 }
--- pet-clinic-ui/src/main/resources/SLING-INF/apps/petclinic/components/pages/visits/edit.groovy
+++ pet-clinic-ui/src/main/resources/SLING-INF/apps/petclinic/components/pages/visits/edit.groovy
@@ -1,15 +1,3 @@
-import org.apache.sling.api.resource.ValueMap
-
-def suffix = request.getRequestPathInfo().getSuffix()
-def resourceResolver = resource.getResourceResolver()
-
-def visitResource = resourceResolver.getResource(suffix)
-def visitProps = visitResource.adaptTo(ValueMap.class)
-def petResource = visitResource.getParent().getParent()
-def petProps = petResource.adaptTo(ValueMap.class)
-def ownerResource = petResource.getParent().getParent()
-def ownerProps = ownerResource.adaptTo(ValueMap.class)
-
 sling.include(resource, 'petclinic/components/header')
 sling.include(resource, 'petclinic/components/navigation')
 
@@ -21,38 +9,7 @@ markupBuilder.html {
           h1(class: 'ui header', 'Add Pet')
         }
       }
-      form(class: 'ui form', role: 'form', action: "${suffix}", method: 'POST') {
-        div(class: 'field') {
-          label(for: 'firstName', 'First Name:')
-          input(id: 'firstName', name: 'firstName', type: 'text', readonly: 'true',
-              value: "${ownerProps.get('firstName')} ${ownerProps.get('lastName')}")
-        }
-        div(class: 'field') {
-          label(for: 'name', 'Name:')
-          input(id: 'name', name: 'name', type: 'text', placeholder: 'Name', readonly: 'true',
-              value: "${petProps.get('name')}")
-        }
-        div(class: 'field') {
-          label(for: 'birthDate', 'Birth Date:')
-          input(id: 'birthDate', name: 'birthDate', type: 'text', placeholder: 'dd/MM/yy', readonly: 'true',
-              value: "${petProps.get('birthDate')}")
-        }
-        div(class: 'field') {
-          label(for: 'date', 'Date:')
-          input(id: 'date', name: 'date', type: 'text', placeholder: 'dd/MM/yy',
-              value: "${visitProps.get('date')}")
-        }
-        div(class: 'field') {
-          label(for: 'description', 'Description:')
-          input(id: 'description', name: 'description', type: 'text', placeholder: 'Description',
-              value: "${visitProps.get('description')}")
-        }
-        input(type: 'hidden', name: 'sling:resourceType', value: 'petclinic/components/pages/owners/pets')
-        input(type: 'hidden', name: ':redirect',
-            value: "/content/petclinic/en/owners.detail.html${ownerResource.getPath()}")
-        input(type: 'hidden', name: '_charset_', value: 'UTF-8')
-        button(type: 'submit', class: 'ui blue submit button', 'Save')
-      }
+      sling.include(resource, 'petclinic/components/visits')
     }
   }
 }
